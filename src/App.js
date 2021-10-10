c1c36d3ce5e7bac6fa67443e829a2a5ded4c8240
AmirAwan <amirawan99925@gmail.com>
2021-10-10 23:20:02 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { Home } from './components/Home'
+import { About } from './components/About'
+import { Contact } from './components/Contact' 
+import { Layout } from './components/Layout'
+import { NavigationBar } from './components/NavigationBar'
+
+function App() {
+  return (
+    <>
+      <NavigationBar />
+      <Layout>  
+        <Router>
+          <Switch>
+            <Route exact={true} path="/" component={Home} />
+            <Route exact={true} path="/about" component={About} />
+            <Route exact={true} path="/contact" component={Contact} />
+          </Switch>
+        </Router>
+      </Layout>  
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Navbar, Nav } from 'react-bootstrap'
+import styled from 'styled-components'
+
+export const NavigationBar = () => {
+
+  const Styles = styled.div`
+    .navbar {
+    	background-color: #222;
+    }
+    .navbar-brand, .navbar-nav .nav-link {
+    	color: #bbb;
+
+    	&:hover {
+    		color: white;
+    	}
+    } 
+  `
+  return(
+  	<Styles>
+	  	<Navbar expand="lg">
+	  	  <Navbar.Brand href="/">Antiques</Navbar.Brand>
+	  	  <Navbar.Toggle aria-controls='basic-navbar-nav' />
+	  	  <Navbar.Collapse id="basic-navbar-nav">
+	        <Nav className="ml-auto">
+	          <Nav.Item><Nav.Link href='/'>Home</Nav.Link></Nav.Item>
+	          <Nav.Item><Nav.Link href='/about'>About</Nav.Link></Nav.Item>
+	          <Nav.Item><Nav.Link href='/contact'>Contact Us</Nav.Link></Nav.Item>
+	        </Nav>
+	  	  </Navbar.Collapse>
+	  	</Navbar>
+  	</Styles>
+  )
+}
